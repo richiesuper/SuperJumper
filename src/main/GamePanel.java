57793d8ddf945c449baa9deb1296b46b584ea4bb
Caknoooo <caknocomel@gmail.com>
2022-12-01 19:57:31 +0700

    feat: add new files for tiling

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -13,6 +13,7 @@ import javax.swing.JPanel;
 import inputs.KeyboardInput;
 import inputs.MouseInput;
 import tilemap.Background;
+import tilemap.TileMap;
 import utils.Constants;
 
 @SuppressWarnings("serial")
@@ -23,6 +24,7 @@ public class GamePanel extends JPanel {
 	private BufferedImage spriteSheet;
 	private BufferedImage[][] spriteTile;
 	private int ticker, idx;
+	private TileMap tm;
 
 	public GamePanel(int width, int height) {
 		super();
@@ -32,12 +34,18 @@ public class GamePanel extends JPanel {
 
 		try {
 			bg = new Background(Constants.Backgrounds.MAIN_MENU);
-			InputStream is = getClass().getResourceAsStream("/entities/player/player.png");
+			InputStream is = getClass().getResourceAsStream(Constants.Entities.Player.SPRITE);
 			this.spriteSheet = ImageIO.read(is);
 
 			for (int i = 0; i < 12; i++)
 				for (int j = 0; j < 7; j++)
 					this.spriteTile[i][j] = this.spriteSheet.getSubimage(j * 128, i * 128, 128, 128);
+
+			tm = new TileMap(32);
+			tm.loadTiles(Constants.Tilesets.LVL_2);
+			tm.loadMap(Constants.Maps.LVL_2);
+			tm.setPosition(0, 0);
+			tm.setTween(1);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -56,7 +64,18 @@ public class GamePanel extends JPanel {
 		ticker++;
 
 		bg.draw(g);
+		tm.draw(g);
 		g.drawImage(spriteTile[1][idx], 0, 0, 128, 128, null);
+//		g.drawImage(spriteTile[2][idx], 0, 128, 128, 128, null);
+//		g.drawImage(spriteTile[3][idx], 0, 256, 128, 128, null);
+//		g.drawImage(spriteTile[4][idx], 0, 384, 128, 128, null);
+//		g.drawImage(spriteTile[5][idx], 128, 0, 128, 128, null);
+//		g.drawImage(spriteTile[6][idx], 128, 128, 128, 128, null);
+//		g.drawImage(spriteTile[7][idx], 128, 256, 128, 128, null);
+//		g.drawImage(spriteTile[8][idx], 128, 384, 128, 128, null);
+//		g.drawImage(spriteTile[9][idx], 256, 0, 128, 128, null);
+//		g.drawImage(spriteTile[10][idx], 256, 128, 128, 128, null);
+//		g.drawImage(spriteTile[11][idx], 256, 256, 128, 128, null);
 
 		if (ticker % 10 == 0) {
 			ticker = 0;
---
+++ src/tilemap/TileMap.java
@@ -0,0 +1,167 @@
+package tilemap;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+import utils.Constants;
+
+public class TileMap {
+	// Position
+	private double x;
+	private double y;
+	private double tween;
+
+	// Tileset
+	private BufferedImage tileset;
+	private int numTilesAcross;
+	private Tile[][] tiles;
+
+	// Drawing
+	private int rowOffset;
+	private int colOffset;
+	private int numRowsToDraw;
+	private int numColsToDraw;
+	private int tileSize;
+
+	// Bounds
+	private int xmin;
+	private int xmax;
+	private int ymin;
+	private int ymax;
+
+	// map
+	private int[][] map;
+	private int numRows;
+	private int numCols;
+	private int width;
+	private int height;
+
+	// loadmap
+	BufferedReader br;
+
+	public TileMap(int tileSize) {
+		this.tileSize = tileSize;
+		this.numRowsToDraw = Constants.Panel.WIDTH / tileSize + 2;
+		this.numColsToDraw = Constants.Panel.HEIGHT / tileSize + 2;
+		this.tween = 0.07;
+	}
+
+	public void loadTiles(String s) {
+		try {
+			InputStream is;
+			is = getClass().getResourceAsStream(s);
+			this.tileset = ImageIO.read(is);
+
+			numTilesAcross = tileset.getWidth() / tileSize;
+			tiles = new Tile[2][numTilesAcross];
+
+			BufferedImage subimage;
+			for (int col = 0; col < numTilesAcross; col++) {
+				subimage = tileset.getSubimage(col * tileSize, 0, tileSize, tileSize);
+				tiles[0][col] = new Tile(subimage, Tile.NORMAL);
+				subimage = tileset.getSubimage(col * tileSize, tileSize, tileSize, tileSize);
+				tiles[1][col] = new Tile(subimage, Tile.BLOCKED);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void loadMap(String s) {
+		try {
+			InputStream is;
+			is = getClass().getResourceAsStream(s);
+			br = new BufferedReader(new InputStreamReader(is));
+
+			numCols = Integer.parseInt(br.readLine());
+			numRows = Integer.parseInt(br.readLine());
+
+			map = new int[numRows][numCols];
+//			System.out.println(numRows);
+//			System.out.println(numCols);
+
+			this.width = numCols * tileSize;
+			this.height = numRows * tileSize;
+
+			xmin = Constants.Panel.WIDTH - this.width;
+			xmax = 0;
+			ymin = Constants.Panel.HEIGHT - this.height;
+			ymax = 0;
+
+			String delims = "\\s+";
+			for (int row = 0; row < numRows; row++) {
+				String line = br.readLine();
+				String[] tokens = line.split(delims);
+				for (int col = 0; col < numCols; col++) {
+					map[row][col] = Integer.parseInt(tokens[col]);
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void setTween(double d) {
+		this.tween = d;
+	}
+
+	public void setPosition(double x, double y) {
+
+		this.x += (x - this.x) * tween;
+		this.y += (y - this.y) * tween;
+
+		fixBounds();
+
+		colOffset = (int) -this.x / tileSize;
+		rowOffset = (int) -this.y / tileSize;
+
+	}
+
+	private void fixBounds() {
+		if (x < xmin)
+			x = xmin;
+		if (y < ymin)
+			y = ymin;
+		if (x > xmax)
+			x = xmax;
+		if (y > ymax)
+			y = ymax;
+	}
+
+	public void draw(Graphics g) {
+
+		for (int row = rowOffset; row < rowOffset + numRowsToDraw; row++) {
+
+			if (row >= numRows)
+				break;
+
+			for (int col = colOffset; col < colOffset + numColsToDraw; col++) {
+
+				if (col >= numCols)
+					break;
+
+				if (map[row][col] == 0)
+					continue;
+
+				int rc = map[row][col];
+				int r = rc / numTilesAcross;
+				int c = rc % numTilesAcross;
+
+				g.drawImage(tiles[r][c].getImage(), (int) x + col * tileSize, (int) y + row * tileSize, null);
+
+			}
+
+		}
+
+	}
+
+}
