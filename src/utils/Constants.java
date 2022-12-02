387750ab21754927aa4ef624fce3e1e5de06e288
richiesuper <richie9963@gmail.com>
2022-12-02 12:57:16 +0700

    refactor: Constants.java
    Refactored many of Constants class' constants data type to reflect sane
    possible ranges.
    
    Commit by richiesuper

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -14,38 +14,38 @@ public class Constants {
 		// game title
 		public static final String TITLE = "Super Jumpers";
 		// please read this as "milliseconds in a second"
-		public static final int MS_IN_S = 1000;
+		public static final short MS_IN_S = 1000;
 		// frame per second
-		public static final int FPS = 60;
+		public static final byte FPS = 60;
 		// period per frame in ms
-		public static final int FRAME_PERIOD = MS_IN_S / FPS;
+		public static final float FRAME_PERIOD = (float) MS_IN_S / (float) FPS;
 	}
-	
+
 	public static class GameStates {
 		// types of game states
-		public static final int MAIN_MENU = 0;
-		public static final int PAUSE_MENU = 1;
-		public static final int OPTIONS_MENU = 2;
-		public static final int GAME_OVER = 3;
-		public static final int GAME_FINISH = 4;
-		public static final int LEVEL_SELECTION = 5;
-		public static final int LVL_1 = 6;
-		public static final int LVL_2 = 7;
+		public static final byte MAIN_MENU = 0;
+		public static final byte PAUSE_MENU = 1;
+		public static final byte OPTIONS_MENU = 2;
+		public static final byte GAME_OVER = 3;
+		public static final byte GAME_FINISH = 4;
+		public static final byte LEVEL_SELECTION = 5;
+		public static final byte LVL_1 = 6;
+		public static final byte LVL_2 = 7;
 	}
 
 	public static class Tile {
 		// tile dimensions
-		public static final int WIDTH = 32;
-		public static final int HEIGHT = 32;
+		public static final byte WIDTH = 32;
+		public static final byte HEIGHT = 32;
 		// tile count
-		public static final int HORIZ_SUM = 32;
-		public static final int VERT_SUM = 18;
+		public static final byte HORIZ_SUM = 32;
+		public static final byte VERT_SUM = 18;
 	}
 
 	public static class Panel {
 		// panel dimensions
-		public static final int WIDTH = Tile.WIDTH * Tile.HORIZ_SUM;
-		public static final int HEIGHT = Tile.HEIGHT * Tile.VERT_SUM;
+		public static final short WIDTH = Tile.WIDTH * Tile.HORIZ_SUM;
+		public static final short HEIGHT = Tile.HEIGHT * Tile.VERT_SUM;
 	}
 
 	public static class Backgrounds {
@@ -67,47 +67,66 @@ public class Constants {
 		public static final String LVL_1 = Folders.MAPS + "level-1.map";
 		public static final String LVL_2 = Folders.MAPS + "level-2.map";
 	}
-
+	
+	public static class Animations {
+		// ticker divider
+		public static final byte TICKER_CYCLE = 10;
+	}
+	
 	public static class Entities {
+		// entity types
+		public static final byte PLAYER = 0;
+		public static final byte ZOMBIE = 0;
+
 		public static class Player {
 			// sprite filename
 			public static final String SPRITE = Folders.PLAYER + "player.png";
-			
+
 			// sprite tile dimensions
-			public static final int SPRITE_WIDTH = 128;
-			public static final int SPRITE_HEIGHT = 128;
-			
+			public static final short SPRITE_WIDTH = 128;
+			public static final short SPRITE_HEIGHT = 128;
+
 			// sprite row and column count
-			public static final int SPRITE_ROWS = 12;
-			public static final int SPRITE_COLS = 7;
-			
+			public static final byte SPRITE_ROWS = 12;
+			public static final byte SPRITE_COLS = 7;
+
 			// sprite tile matrix properties
-			public static final int IDLE_TILE_COUNT = 4;
-			public static final int WALK_TILE_COUNT = 7;
-			public static final int RUN_TILE_COUNT = 7;
-			public static final int JUMP_TILE_COUNT = 6;
-			public static final int ATK_1_TILE_COUNT = 5;
-			public static final int ATK_2_TILE_COUNT = 4;
-			public static final int ATK_3_TILE_COUNT = 4;
-			public static final int ATK_RUN_TILE_COUNT = 6;
-			public static final int DEFEND_TILE_COUNT = 5;
-			public static final int PROTECT_TILE_COUNT = 1;
-			public static final int HURT_TILE_COUNT = 2;
-			public static final int DEAD_TILE_COUNT = 6;
+			public static final byte IDLE_TILE_COUNT = 4;
+			public static final byte WALK_TILE_COUNT = 7;
+			public static final byte RUN_TILE_COUNT = 7;
+			public static final byte JUMP_TILE_COUNT = 6;
+			public static final byte ATK_1_TILE_COUNT = 5;
+			public static final byte ATK_2_TILE_COUNT = 4;
+			public static final byte ATK_3_TILE_COUNT = 4;
+			public static final byte ATK_RUN_TILE_COUNT = 6;
+			public static final byte DEFEND_TILE_COUNT = 5;
+			public static final byte PROTECT_TILE_COUNT = 1;
+			public static final byte HURT_TILE_COUNT = 2;
+			public static final byte DEAD_TILE_COUNT = 6;
+			
+			// player direction
+			public static final byte DIR_LEFT = 0;
+			public static final byte DIR_RIGHT = 1;
 
 			// player states
-			public static final int IDLE = 0;
-			public static final int WALK = 1;
-			public static final int RUN = 2;
-			public static final int JUMP = 3;
-			public static final int ATK_1 = 4;
-			public static final int ATK_2 = 5;
-			public static final int ATK_3 = 6;
-			public static final int ATK_RUN = 7;
-			public static final int DEFEND = 8;
-			public static final int PROTECT = 9;
-			public static final int HURT = 10;
-			public static final int DEAD = 11;
+			public static final byte IDLE = 0;
+			public static final byte WALK = 1;
+			public static final byte RUN = 2;
+			public static final byte JUMP = 3;
+			public static final byte ATK_1 = 4;
+			public static final byte ATK_2 = 5;
+			public static final byte ATK_3 = 6;
+			public static final byte ATK_RUN = 7;
+			public static final byte DEFEND = 8;
+			public static final byte PROTECT = 9;
+			public static final byte HURT = 10;
+			public static final byte DEAD = 11;
+			
+			// player stats
+			public static final float DEFAULT_HEALTH = 1000f;
+			public static final float DEFAULT_WALK_SPEED = 0.5f;
+			public static final float DEFAULT_RUN_SPEED = DEFAULT_WALK_SPEED * 2.5f;
+			public static final float DEFAULT_JUMP_HEIGHT = Tile.HEIGHT * 2.5f;
 		}
 
 		public static class Enemies {
@@ -116,36 +135,36 @@ public class Constants {
 				public static final String SPRITE = Folders.ENEMIES + "zombie.png";
 
 				// sprite tile dimensions
-				public static final int SPRITE_WIDTH = 96;
-				public static final int SPRITE_HEIGHT = 96;
+				public static final short SPRITE_WIDTH = 96;
+				public static final short SPRITE_HEIGHT = 96;
 
 				// sprite row and column count
-				public static final int SPRITE_ROWS = 10;
-				public static final int SPRITE_COLS = 11;
+				public static final byte SPRITE_ROWS = 10;
+				public static final byte SPRITE_COLS = 11;
 
 				// sprite tile matrix properties
-				public static final int IDLE_TILE_COUNT = 9;
-				public static final int WALK_TILE_COUNT = 10;
-				public static final int RUN_TILE_COUNT = 8;
-				public static final int JUMP_TILE_COUNT = 6;
-				public static final int ATK_1_TILE_COUNT = 4;
-				public static final int ATK_2_TILE_COUNT = 4;
-				public static final int ATK_3_TILE_COUNT = 4;
-				public static final int EATING_TILE_COUNT = 11;
-				public static final int HURT_TILE_COUNT = 5;
-				public static final int DEAD_TILE_COUNT = 5;
+				public static final byte IDLE_TILE_COUNT = 9;
+				public static final byte WALK_TILE_COUNT = 10;
+				public static final byte RUN_TILE_COUNT = 8;
+				public static final byte JUMP_TILE_COUNT = 6;
+				public static final byte ATK_1_TILE_COUNT = 4;
+				public static final byte ATK_2_TILE_COUNT = 4;
+				public static final byte ATK_3_TILE_COUNT = 4;
+				public static final byte EATING_TILE_COUNT = 11;
+				public static final byte HURT_TILE_COUNT = 5;
+				public static final byte DEAD_TILE_COUNT = 5;
 
 				// zombie states
-				public static final int IDLE = 0;
-				public static final int WALK = 1;
-				public static final int RUN = 2;
-				public static final int JUMP = 3;
-				public static final int ATK_1 = 4;
-				public static final int ATK_2 = 5;
-				public static final int ATK_3 = 6;
-				public static final int EATING = 7;
-				public static final int HURT = 8;
-				public static final int DEAD = 9;
+				public static final byte IDLE = 0;
+				public static final byte WALK = 1;
+				public static final byte RUN = 2;
+				public static final byte JUMP = 3;
+				public static final byte ATK_1 = 4;
+				public static final byte ATK_2 = 5;
+				public static final byte ATK_3 = 6;
+				public static final byte EATING = 7;
+				public static final byte HURT = 8;
+				public static final byte DEAD = 9;
 			}
 		}
 	}
