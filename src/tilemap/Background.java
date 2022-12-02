ce8b5602cf44522f9bf4af2a5447d6c0cb9a71ea
richiesuper <richie9963@gmail.com>
2022-12-02 22:36:11 +0700

    feat: make background flip
    For example, the background to the right of the original background
    should flip itself to hide nasty boundary lines between them
    
    Commit by richiesuper

--- src/tilemap/Background.java
+++ src/tilemap/Background.java
@@ -15,34 +15,49 @@ public class Background {
 	private float x;
 	private float y;
 	private double moveScale;
+	private short ticker;
 
 	public Background(String filename) {
+		// prepare for the worst
 		this.bg = null;
 		this.x = 0;
 		this.y = 0;
 		this.moveScale = 0.1;
-		
+		// for debugging
+		this.ticker = 0;
+
 		InputStream is;
+
 		try {
 			is = getClass().getResourceAsStream(filename);
 			this.bg = ImageIO.read(is);
-			int[] bgColor = this.bg.getRGB(0, 0, this.bg.getWidth(), this.bg.getHeight(), null, 0, this.bg.getWidth() * this.bg.getHeight() );
-			//System.out.println(bgColor[1]);
-			//System.out.println( this.bg.getHeight());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void setPosition(double x, double y) {
 		this.x = (float) ((x * moveScale) % Constants.Panel.WIDTH);
 		this.y = (float) ((y * moveScale) % Constants.Panel.HEIGHT);
 	}
 
 	public void draw(Graphics g) {
+		// debugging
+		ticker++;
+		if (ticker >= 20) {
+			System.out.println("x: " + x + " y: " + y);
+			ticker = 0;
+		}
+
+		// draw the first background "pane" until it finally disappears while going to the left...
 		g.drawImage(bg, (int) x, (int) y, Constants.Panel.WIDTH, Constants.Panel.HEIGHT, null);
-		
-		if(x < 0) g.drawImage(bg, (int) x + Constants.Panel.WIDTH, (int) y, Constants.Panel.WIDTH, Constants.Panel.HEIGHT, null);
-		if(x > 0) g.drawImage(bg, (int) x - Constants.Panel.WIDTH, (int) y, Constants.Panel.WIDTH, Constants.Panel.HEIGHT, null);
+
+		if (x < 0)
+			// draw the second pane to the right, while also REVERSING THE BACKGROUND TO MAKE IT PRETTY!
+			// notice the multiplication by 2 and MINUS SIGN on the Constants.Panel.WIDTH!
+			g.drawImage(bg, (int) x + 2 * Constants.Panel.WIDTH, (int) y, -Constants.Panel.WIDTH, Constants.Panel.HEIGHT, null);
+		else if (x > 0)
+			// draw the pane to the left of original pane
+			g.drawImage(bg, (int) x - Constants.Panel.WIDTH, (int) y, Constants.Panel.WIDTH, Constants.Panel.HEIGHT, null);
 	}
 }
