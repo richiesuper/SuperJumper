f030640aeb04a3c049d106cd1747dc9ea8938fef
richiesuper <richie9963@gmail.com>
2022-12-02 22:35:05 +0700

    refactor: GamePanel.java
    Commit by richiesuper

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -18,10 +18,16 @@ public class GamePanel extends JPanel {
 	private Background bg;
 	private TileMap tm;
 
+	private short halfWidth;
+	private short halfHeight;
+
 	private Player player;
 
 	public GamePanel(int width, int height) {
 		super();
+
+		this.halfWidth = Constants.Panel.WIDTH / 2;
+		this.halfWidth = Constants.Panel.HEIGHT / 2;
 
 		this.player = new Player(0, Constants.Panel.HEIGHT - Constants.Entities.Player.SPRITE_HEIGHT);
 
@@ -29,13 +35,12 @@ public class GamePanel extends JPanel {
 		this.mouseInput = new MouseInput(this);
 
 		bg = new Background(Constants.Backgrounds.LVL_1);
-		
-		tm = new TileMap(32);
+
+		tm = new TileMap(Constants.Tile.WIDTH, Constants.Tile.HEIGHT);
 		tm.loadTiles(Constants.TileSets.LVL_1);
 		tm.loadMap(Constants.Maps.LVL_1);
 		tm.setPosition(0, 0);
 		tm.setTween(1);
-		
 
 		setPreferredSize(new Dimension(width, height));
 		addKeyListener(this.keyboardInput);
@@ -45,16 +50,16 @@ public class GamePanel extends JPanel {
 	public Player getPlayer() {
 		return player;
 	}
-	
+
 	public void update() {
-		tm.setPosition(Constants.Panel.WIDTH / 2 - player.getX(), Constants.Panel.HEIGHT / 2 - player.getY());
-		bg.setPosition(tm.getx(), tm.gety());
+		tm.setPosition(halfWidth - player.getX(), halfHeight - player.getY());
+		bg.setPosition(tm.getX(), tm.getY());
 	}
 
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
+
 		update();
 		bg.draw(g);
 		tm.draw(g);
