d646d1b00b71220c442790ba0e4a1ba4ee95c693
Caknoooo <caknocomel@gmail.com>
2022-12-02 07:39:29 +0700

    test: Test the player's movement

--- src/inputs/KeyboardInput.java
+++ src/inputs/KeyboardInput.java
@@ -7,6 +7,10 @@ import main.GamePanel;
 
 public class KeyboardInput implements KeyListener {
 	private GamePanel gamePanel;
+	public static final int LEFT = 0;
+	public static final int UP = 1;
+	public static final int RIGHT = 2;
+	public static final int DOWN = 3;
 
 	public KeyboardInput(GamePanel gamePanel) {
 		this.gamePanel = gamePanel;
@@ -19,12 +23,34 @@ public class KeyboardInput implements KeyListener {
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		System.out.println("Key " + e.getKeyChar() + " was pressed!");
+		//System.out.println("Key " + e.getKeyChar() + " was pressed!");
+		switch(e.getKeyCode()) {
+			case KeyEvent.VK_W:
+				gamePanel.setDirection(UP);
+				break;
+			case KeyEvent.VK_A:
+				gamePanel.setDirection(LEFT);
+				break;
+			case KeyEvent.VK_S:
+				gamePanel.setDirection(DOWN);
+				break;
+			case KeyEvent.VK_D:
+				gamePanel.setDirection(RIGHT);
+				break;
+		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		System.out.println("Key " + e.getKeyChar() + " was released!");
+		switch(e.getKeyCode()) {
+			case KeyEvent.VK_W:
+			case KeyEvent.VK_A:
+			case KeyEvent.VK_S:
+			case KeyEvent.VK_D:
+				gamePanel.setMoving(false);
+				break;
+		}
 	}
 
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -23,15 +23,38 @@ public class GamePanel extends JPanel {
 	private BufferedImage spriteSheet;
 	private BufferedImage[][] spriteTile;
 	private int ticker, idx;
+	
+	// Tambah
+	private int playerDir;
+	private int playerAction;
+	private float xDelta, yDelta;
+	private static final int IDLE = 0;
+	private static final int RUNNING = 1;
+	private boolean moving;
+	
+	// Temp
+	public static final int LEFT = 0;
+	public static final int UP = 1;
+	public static final int RIGHT = 2;
+	public static final int DOWN = 3;
 
 	public GamePanel(int width, int height) {
 		super();
+		
+		// Add
+		this.playerDir = -1;
+		this.moving = false;
+		this.playerAction = IDLE;
+		this.xDelta = 100;
+		this.yDelta = 100;
+		
+		
 		this.keyboardInput = new KeyboardInput(this);
 		this.mouseInput = new MouseInput(this);
 		this.spriteTile = new BufferedImage[12][7];
 
 		try {
-			bg = new Background(Constants.Backgrounds.MAIN_MENU);
+			bg = new Background(Constants.Backgrounds.LVL_1);
 			InputStream is = getClass().getResourceAsStream(Constants.Entities.Player.SPRITE);
 			this.spriteSheet = ImageIO.read(is);
 
@@ -52,23 +75,76 @@ public class GamePanel extends JPanel {
 		ticker = 0;
 		idx = 0;
 	}
+	
+	public void setDirection(int direction) {
+		this.playerDir = direction;
+		moving = true;
+	}
+	
+	public void setMoving(boolean moving) {
+		this.moving = moving;
+	}
+	
+	private void setAnimation() {
+		if(moving)
+			playerAction = RUNNING;
+		else
+			playerAction = IDLE;
+
+	}
+	
+	private void updatePos() {
+		if(moving) {
+			switch(playerDir) {
+				case LEFT:
+					xDelta -= 5;
+					break;
+				case UP:
+					yDelta -= 5;
+					break;
+				case RIGHT:
+					xDelta += 5;
+					break;
+				case DOWN:
+					yDelta += 5;
+					break;
+			}
+		}
+	}
 
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		ticker++;
-
+		
 		bg.draw(g);
-		g.drawImage(spriteTile[1][idx], 0, 0, Constants.Entities.Player.SPRITE_WIDTH, Constants.Entities.Player.SPRITE_HEIGHT, null);
+		setAnimation();
+		updatePos();
+		if(playerAction == IDLE) {
+			g.drawImage(spriteTile[playerAction][idx], (int) xDelta, (int) yDelta, Constants.Entities.Player.SPRITE_WIDTH, Constants.Entities.Player.SPRITE_HEIGHT, null);
 
-		if (ticker % 10 == 0) {
-			ticker = 0;
+			if (ticker % 10 == 0) {
+				ticker = 0;
 
-			if (idx >= 6) {
-				idx = 0;
+				if (idx >= 3) {
+					idx = 0;
+				}
+
+				idx++;
+			}	
+		}
+		else {
+			g.drawImage(spriteTile[playerAction][idx], (int) xDelta, (int) yDelta, Constants.Entities.Player.SPRITE_WIDTH, Constants.Entities.Player.SPRITE_HEIGHT, null);
+			
+			if (ticker % 10 == 0) {
+				ticker = 0;
+				
+				if (idx >= 6) {
+					idx = 0;
+				}
+				
+				idx++;
 			}
-
-			idx++;
 		}
 	}
 }
