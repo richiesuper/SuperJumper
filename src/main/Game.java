79b142bde7065a2a8bc8cbcf3e59343183703f56
richiesuper <richie9963@gmail.com>
2022-12-17 17:11:37 +0700

    fix: changes with the GameStateManager object in Game class
    also some refactoring too... (delete unused methods).
    
    Commit by richiesuper

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,6 +1,5 @@
 package main;
 
-import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
@@ -14,17 +13,17 @@ public class Game implements Runnable {
 	private Thread gameThread;
 	private GameStateManager gsm;
 	private MainMenuState menu;
-	private boolean running;
-	
+
 	// image
 	private Graphics2D g;
 	private BufferedImage img;
 
 	public Game() {
-		this.gamePanel = new GamePanel(Constants.Panel.WIDTH, Constants.Panel.HEIGHT);
+		this.gsm = new GameStateManager();
+		this.gamePanel = new GamePanel(Constants.Panel.WIDTH, Constants.Panel.HEIGHT, gsm);
 		this.gameFrame = new GameFrame(Constants.Game.TITLE, this.gamePanel);
 		this.gamePanel.requestFocus();
-		
+
 		startGameLoop();
 	}
 
@@ -32,46 +31,17 @@ public class Game implements Runnable {
 		this.gameThread = new Thread(this);
 		this.gameThread.start();
 	}
-	
-	public void init() {
-		img = new BufferedImage(Constants.Panel.WIDTH, Constants.Panel.HEIGHT, BufferedImage.TYPE_INT_RGB);
-		
-		g = (Graphics2D) img.getGraphics();
-		
-		running = true;
-		
-		gsm = new GameStateManager();
-	}
 
 	@Override
 	public void run() {
-		init();
-		while (running) {
+		while (true) {
 			gamePanel.repaint();
-			
-			update();
-			draw();
-			drawToScreen();
-			
+
 			try {
 				Thread.sleep((long) Constants.Game.FRAME_PERIOD);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
-	}
-
-	private void drawToScreen() {
-		Graphics g2 = img.getGraphics();
-		g2.drawImage(img, 0, 0, Constants.Panel.WIDTH, Constants.Panel.HEIGHT, null);
-		g2.dispose();
-	}
-
-	private void draw() {
-		gsm.draw(g);
-	}
-
-	private void update() {
-		gsm.update();
 	}
 }
