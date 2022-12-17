6bc4dce57c6871809a7abfc994774d7a29e56407
Caknoooo <caknocomel@gmail.com>
2022-12-17 10:17:21 +0700

    feat: added an enum method for menustate game and interface for help methods
    commit by Caknoooo

---
+++ src/gamestates/EnumState.java
@@ -0,0 +1,7 @@
+package gamestates;
+
+public enum EnumState {
+	PLAYING, MENU, OPTIONS, QUIT;
+	
+	public static EnumState state = MENU;
+}
---
+++ src/gamestates/StateMethods.java
@@ -0,0 +1,18 @@
+package gamestates;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.KeyEvent;
+
+public interface StateMethods {
+	public void mouseClicked(MouseEvent e);
+
+	public void mousePressed(MouseEvent e);
+
+	public void mouseReleased(MouseEvent e);
+
+	public void mouseMoved(MouseEvent e);
+//
+//	public void keyPressed(KeyEvent e);
+//
+//	public void keyReleased(KeyEvent e);
+}
