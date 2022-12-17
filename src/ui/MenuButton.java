bdb4a1feb28baf00601f7984b93b7c34247337d6
richiesuper <richie9963@gmail.com>
2022-12-17 12:43:39 +0700

    fix: prevented a index out of bounds exception in MenuButton class
    previously, there are 3 button states, mouseOver, mousePressed, and
    none.
    But our assets only has 2 types, the unpressed button and pressed
    button.
    So, the mouseOver state is scrapped!
    
    there are also some code refactoring and style fixes though...
    
    Commit by richiesuper

--- src/ui/MenuButton.java
+++ src/ui/MenuButton.java
@@ -1,5 +1,9 @@
 package ui;
 
+import static utils.Constants.UI.Button.B_HEIGHT;
+import static utils.Constants.UI.Button.B_WIDTH;
+import static utils.Constants.UI.Button.MENU_BUTTON;
+
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
@@ -9,24 +13,23 @@ import java.io.InputStream;
 import javax.imageio.ImageIO;
 
 import gamestates.EnumState;
-import static utils.Constants.UI.Button.*;
 
 public class MenuButton {
 	private int xPos, yPos;
 	private int rowIndex, index;
 	private int xOffsetCenter = 140;
-	
+
 	private EnumState state;
 	private BufferedImage[] imgs;
-	private boolean mouseOver, mousePressed;
+	private boolean mousePressed;
 	private Rectangle bounds;
-	
+
 	public MenuButton(int xPos, int yPos, int rowIndex, EnumState state) {
 		this.xPos = xPos;
 		this.yPos = yPos;
 		this.rowIndex = rowIndex;
 		this.state = state;
-		
+
 		loadImgs();
 		initBounds();
 	}
@@ -34,46 +37,39 @@ public class MenuButton {
 	private void loadImgs() {
 		imgs = new BufferedImage[2];
 		BufferedImage temp = null;
-		
+
 		InputStream is = getClass().getResourceAsStream(MENU_BUTTON);
 		System.out.println(is);
-		
+		System.out.println("TEST");
+
 		try {
 			temp = ImageIO.read(is);
-		} catch(IOException e) {
+			System.out.println(temp);
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		// Errornya disini, dia ga nangkep gambarnya
-		
-//		for(int i = 0; i < imgs.length; i++) {
-//			imgs[i] = temp.getSubimage(i * B_WIDTH, rowIndex * B_HEIGHT, B_WIDTH, B_HEIGHT);
-//		}
-		
+
+		for (int i = 0; i < imgs.length; i++) {
+			imgs[i] = temp.getSubimage(i * B_WIDTH, rowIndex * B_HEIGHT, B_WIDTH, B_HEIGHT);
+		}
+
 	}
 
 	private void initBounds() {
 		bounds = new Rectangle(xPos - xOffsetCenter, yPos, B_WIDTH, B_HEIGHT);
 	}
-	
+
 	public void draw(Graphics g) {
 		g.drawImage(imgs[index], xPos - xOffsetCenter, yPos, B_WIDTH, B_HEIGHT, null);
 	}
-	
+
 	public void update() {
-		index = 0;
-		if(mouseOver)
+		if (mousePressed)
 			index = 1;
-		if(mousePressed)
-			index = 2;
-	}
-
-	public boolean isMouseOver() {
-		return mouseOver;
-	}
-
-	public void setMouseOver(boolean mouseOver) {
-		this.mouseOver = mouseOver;
+		else
+			index = 0;
 	}
 
 	public boolean isMousePressed() {
@@ -83,19 +79,17 @@ public class MenuButton {
 	public void setMousePressed(boolean mousePressed) {
 		this.mousePressed = mousePressed;
 	}
-	
+
 	public Rectangle getBounds() {
 		return bounds;
 	}
-	
+
 	public void applyEnumstate() {
 		EnumState.state = state;
 	}
-	
+
 	public void resetBools() {
-		mouseOver = false;
 		mousePressed = false;
 	}
-	
-	
+
 }
