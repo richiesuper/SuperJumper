238d4f4a81c5df1994e6186ab3fe768b12646451
richiesuper <richie9963@gmail.com>
2022-12-17 12:36:43 +0700

    fix: prevent NullPointerException bug from happening in KeyboardInput.java
    Only get the player if the current game state INDEED HAS a player inside
    it!
    Game states like main menu, options, etc, doesn't have a player inside
    it, so we prevent them from being called to return a player object.
    
    Commit by richiesuper

--- src/inputs/KeyboardInput.java
+++ src/inputs/KeyboardInput.java
@@ -20,70 +20,74 @@ public class KeyboardInput implements KeyListener {
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		switch (e.getKeyChar()) {
-		case 'a':
-			gamePanel.getPlayer().setDirection(Constants.Entities.Player.DIR_LEFT);
-			gamePanel.getPlayer().setMoving(true);
-			gamePanel.getPlayer().setState(Constants.Entities.Player.WALK);
-			break;
-		case 'A':
-			gamePanel.getPlayer().setDirection(Constants.Entities.Player.DIR_LEFT);
-			gamePanel.getPlayer().setMoving(true);
-			gamePanel.getPlayer().setState(Constants.Entities.Player.RUN);
-			break;
-		case 'd':
-			gamePanel.getPlayer().setDirection(Constants.Entities.Player.DIR_RIGHT);
-			gamePanel.getPlayer().setMoving(true);
-			gamePanel.getPlayer().setState(Constants.Entities.Player.WALK);
-			break;
-		case 'D':
-			gamePanel.getPlayer().setDirection(Constants.Entities.Player.DIR_RIGHT);
-			gamePanel.getPlayer().setMoving(true);
-			gamePanel.getPlayer().setState(Constants.Entities.Player.RUN);
-			break;
-		case 'w':
-			gamePanel.getPlayer().setDirection(Constants.Entities.Player.DIR_UP);
-			gamePanel.getPlayer().setMoving(true);
-			gamePanel.getPlayer().setState(Constants.Entities.Player.WALK);
-			break;
-		case 'W':
-			gamePanel.getPlayer().setDirection(Constants.Entities.Player.DIR_UP);
-			gamePanel.getPlayer().setMoving(true);
-			gamePanel.getPlayer().setState(Constants.Entities.Player.RUN);
-			break;
-		case 's':
-			gamePanel.getPlayer().setDirection(Constants.Entities.Player.DIR_DOWN);
-			gamePanel.getPlayer().setMoving(true);
-			gamePanel.getPlayer().setState(Constants.Entities.Player.WALK);
-			break;
-		case 'S':
-			gamePanel.getPlayer().setDirection(Constants.Entities.Player.DIR_DOWN);
-			gamePanel.getPlayer().setMoving(true);
-			gamePanel.getPlayer().setState(Constants.Entities.Player.RUN);
-			break;
-		default:
-			break;
+		if (gamePanel.getGsm().getCurrState() >= Constants.GameStates.LEVEL_SELECTION) {
+			switch (e.getKeyChar()) {
+			case 'a':
+				gamePanel.getGsm().getGameState().getPlayer().setDirection(Constants.Entities.Player.DIR_LEFT);
+				gamePanel.getGsm().getGameState().getPlayer().setMoving(true);
+				gamePanel.getGsm().getGameState().getPlayer().setState(Constants.Entities.Player.WALK);
+				break;
+			case 'A':
+				gamePanel.getGsm().getGameState().getPlayer().setDirection(Constants.Entities.Player.DIR_LEFT);
+				gamePanel.getGsm().getGameState().getPlayer().setMoving(true);
+				gamePanel.getGsm().getGameState().getPlayer().setState(Constants.Entities.Player.RUN);
+				break;
+			case 'd':
+				gamePanel.getGsm().getGameState().getPlayer().setDirection(Constants.Entities.Player.DIR_RIGHT);
+				gamePanel.getGsm().getGameState().getPlayer().setMoving(true);
+				gamePanel.getGsm().getGameState().getPlayer().setState(Constants.Entities.Player.WALK);
+				break;
+			case 'D':
+				gamePanel.getGsm().getGameState().getPlayer().setDirection(Constants.Entities.Player.DIR_RIGHT);
+				gamePanel.getGsm().getGameState().getPlayer().setMoving(true);
+				gamePanel.getGsm().getGameState().getPlayer().setState(Constants.Entities.Player.RUN);
+				break;
+			case 'w':
+				gamePanel.getGsm().getGameState().getPlayer().setDirection(Constants.Entities.Player.DIR_UP);
+				gamePanel.getGsm().getGameState().getPlayer().setMoving(true);
+				gamePanel.getGsm().getGameState().getPlayer().setState(Constants.Entities.Player.WALK);
+				break;
+			case 'W':
+				gamePanel.getGsm().getGameState().getPlayer().setDirection(Constants.Entities.Player.DIR_UP);
+				gamePanel.getGsm().getGameState().getPlayer().setMoving(true);
+				gamePanel.getGsm().getGameState().getPlayer().setState(Constants.Entities.Player.RUN);
+				break;
+			case 's':
+				gamePanel.getGsm().getGameState().getPlayer().setDirection(Constants.Entities.Player.DIR_DOWN);
+				gamePanel.getGsm().getGameState().getPlayer().setMoving(true);
+				gamePanel.getGsm().getGameState().getPlayer().setState(Constants.Entities.Player.WALK);
+				break;
+			case 'S':
+				gamePanel.getGsm().getGameState().getPlayer().setDirection(Constants.Entities.Player.DIR_DOWN);
+				gamePanel.getGsm().getGameState().getPlayer().setMoving(true);
+				gamePanel.getGsm().getGameState().getPlayer().setState(Constants.Entities.Player.RUN);
+				break;
+			default:
+				break;
+			}
 		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		System.out.println("Key " + e.getKeyChar() + " was released!");
-		switch (e.getKeyChar()) {
-		case 'w':
-		case 'W':
-		case 's':
-		case 'S':
-		case 'a':
-		case 'A':
-		case 'd':
-		case 'D':
-			gamePanel.getPlayer().setMoving(false);
-			gamePanel.getPlayer().setState(Constants.Entities.Player.IDLE);
-			break;
-		default:
-			break;
+
+		if (gamePanel.getGsm().getCurrState() >= Constants.GameStates.LEVEL_SELECTION) {
+			switch (e.getKeyChar()) {
+			case 'w':
+			case 'W':
+			case 's':
+			case 'S':
+			case 'a':
+			case 'A':
+			case 'd':
+			case 'D':
+				gamePanel.getGsm().getGameState().getPlayer().setMoving(false);
+				gamePanel.getGsm().getGameState().getPlayer().setState(Constants.Entities.Player.IDLE);
+				break;
+			default:
+				break;
+			}
 		}
 	}
-
 }
